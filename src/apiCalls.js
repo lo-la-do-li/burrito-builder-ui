9dd660dec5ed13de0d2b599c50ddbc789da05a80
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-01-19 09:54:30 -0700

    Add postOrder method to apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,20 @@
 export const getOrders = () => {
-  return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
-}
+  return fetch("http://localhost:3001/api/v1/orders").then((response) =>
+    response.json()
+  );
+};
+
+export const postOrder = (name, ingredients) => {
+  return fetch("http://localhost:3001/api/v1/orders", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      ingredients: ingredients,
+    }),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+};
